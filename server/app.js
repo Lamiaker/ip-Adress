59d5f24493510622e11d1f67df128ac1aa3b3f1f
lamia_ker <btcb331@gmail.com>
2024-09-21 15:01:46 +0100

    ajouter un text

--- server/app.js
+++ server/app.js
@@ -11,8 +11,13 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   // Retrieve the IP address
-  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  // const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
+  // res.send(`Your IP address is: ${ip}`);
+
+  const ip = (req.headers["x-forwarded-for"] || req.socket.remoteAddress).split(
+    ","
+  )[0];
   res.send(`Your IP address is: ${ip}`);
 });
 
